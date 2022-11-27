986be806f65b9e5b424c72f15c74ccdb7d868078
Matthew <matthew.thomas@jp.kpmg.com>
2022-11-27 22:52:43 +0900

    add content switching logic to index.js and start to build menu page module logic

---
+++ src/menu-page.js
@@ -0,0 +1,29 @@
+import './style.css';
+
+class menuItem {
+    constructor(name, description, price, pictureURL){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.pictureURL = pictureURL;
+    }
+
+}
+
+function menuPage() {
+    const menuItems = [];
+    const triniCurryChicken = new menuItem("Trinidadian Curry Chicken", "This is a staple dish in trinidad enjoyed in restaurants and homes alike. Made with a wide assortment of herbs and spices. Definitely will delight your taste buds!", "1000 YEN", )
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+export { menuPage };
